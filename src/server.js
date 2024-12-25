ee611f8f32e2791699ed45e1bb8eeb3637b0d2aa
SoloScripter3 <vardhanv6259@gmail.com>
2024-12-25 20:50:27 +0530

    added comments

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+//initializing express
 const app = express();
+
+//loading env variables
 dotenv.config();
+
+//using json
+app.use(express.json());
+
+//connecting db to the application
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//defining port
 const port = process.env.PORT || 3000;
 
+//listening to the port
 app.listen(port, () => console.log(`http://localhost:${port}`));
