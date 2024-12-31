267498c17edfaec9abdf7f90ddc78766a999b471
SoloScripter3 <vardhanv6259@gmail.com>
2024-12-31 23:49:26 +0530

    changed port for testing

--- src/utils/otpSender.js
+++ src/utils/otpSender.js
@@ -9,8 +9,8 @@ const otpGenerator = () => {
 //creating transporter to send the mail
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.APP_PASSWORD,
